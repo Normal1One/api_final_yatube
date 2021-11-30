c32d3d4faac2eeb699a562338e18a5fe8612ccee
Normal_One <88935539+Normal1One@users.noreply.github.com>
2021-11-30 22:48:12 +0300

    Update serializers.py

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -2,7 +2,6 @@ from django.contrib.auth import get_user_model
 from rest_framework import serializers
 from rest_framework.relations import SlugRelatedField
 
-
 from posts.models import Comment, Post, Follow, Group
 
 User = get_user_model()
